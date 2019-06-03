e83b5349e274e401c3f2c956f05c65e2297c142b
Vibhav-deo <vdeo@deakin.edu.au>
2019-06-03 17:44:09 +1000

    updated card to have data according to current backend api

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -10,14 +10,39 @@ class Cards extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      notifyAgent: false,
+      data: []
     };
+
+    this.notifyAgent = this.notifyAgent.bind(this)
   }
 
+  notifyAgent() {
+    this.setState({notifyAgent: true})
+    let data = {
+      id: this.props.data.id,
+      title: this.props.data.Title,
+      university: this.props.data.university
+    }
+    let temp = this.props.parentState.courseData
+    temp.push(data)
+    this.props.parentStateHandler({courseData: temp})
+    console.log('[PARENT STATE]',this.props.parentState)
+    //window.localStorage.setItem("agentData", data)
+  }
+
+  componentDidMount() {
+    this.setState({
+      students: window.localStorage.getItem("students") || "[]"
+    })
+    if(this.props.data > 0)
+    this.setState({
+      data: this.props.data
+    })
+  }
   render() {
-
+    console.log('[PROPS]',this.props.data)
     if (this.props.Ctype === "Cdesc" && this.props.previousField === this.props.data.coursetype) {
-
       return (
         <div className="card">
           <div className="card-content valign-wrapper">
@@ -28,27 +53,31 @@ class Cards extends Component {
                     <h5>{this.props.data.Title}</h5>
                   </div>
                   <div className="col l12 m12 s12">
-                    <div className="">{this.props.data.University}</div>
+                    <div className="">{this.props.data.university.name}</div>
                   </div>
                   <div className="col l12 m12 s12">
-                    <div className="">Course Level: {
-                      (this.props.data.degreelevel.toLowerCase() === "master" ? "Master" : (
-                        this.props.data.degreelevel.toLowerCase() === "phd" ? "Doctrate" : (
-                          this.props.data.degreelevel.toLowerCase() === "diploma" ? "Diploma" :
-                            (this.props.data.degreelevel.toLowerCase() === "bachelor" ? "Bachelor" : ''))))
+                    <div className="">Course Level: {this.props.data.courseLevel
+                      // (this.props.data.courseLevel.toLowerCase() === "master" ? "Master" : (
+                      //   this.props.data.courseLevel.toLowerCase() === "phd" ? "Doctrate" : (
+                      //     this.props.data.courseLevel.toLowerCase() === "diploma" ? "Diploma" :
+                      //       (this.props.data.courseLevel.toLowerCase() === "bachelor" ? "Bachelor" : ''))))
                     }</div>
+                  </div>
+                  <div className="col l12 m12 s12">
+                    {this.props.data.description}
                   </div>
                 </div>
               </div>
               <div className="col l4 m0 s0">
-                <UniLOGO uniname={this.props.data.University} key={Date.now()} />
+                <UniLOGO uniname={this.props.data.university.name} key={Date.now()} />
 
               </div>
 
             </div>
           </div>
-          <div className="card-action left-align">
+          <div className="card-action">
             <Link className="red-text" to={{ pathname: '/courseDetail', params: this.props.data }}>Details</Link>
+            {this.state.notifyAgent === false ? <Link className="red-text" to={{ params: this.props.data }} onClick={this.notifyAgent}>Send your expression of Interest</Link> : <Link className="red-text" to={{ params: this.props.data }}>Sent</Link>}
           </div>
         </div>
       );
@@ -58,7 +87,7 @@ class Cards extends Component {
         <div className="card darken-1">
           <div className="card-content black-text">
             <span className="card-title left-align">{this.props.data.name}</span>
-            <p className="left-align">
+            <p className="truncate left-align">
               {this.props.data.desc}
             </p>
           </div>
@@ -80,6 +109,11 @@ class Cards extends Component {
                 <p>
                   {this.props.data.desc}
                 </p>
+              </div>
+              <div className="card-action center red-text">
+                {
+                  this.props.link !== undefined ? <Link className="red-text" to={{ pathname: '/home', params: this.props.data }}>View Courses</Link> : null
+                }
               </div>
             </div>
           </div>
@@ -105,27 +139,27 @@ class Cards extends Component {
       return (
         <div className="card">
           <div className="card-image waves-effect waves-block waves-light">
-            <img alt="adv-img" className="activator" src="https://mumbrella.com.au/wp-content/uploads/2016/09/18_Deakin_Hero1_Billboard_8x13-1.jpg"/>
-        </div>
-            <div className="card-content">
-              <span className="card-title activator grey-text text-darken-4">Deakin University<i className="material-icons right">more_vert</i></span>
-              <p>Think Young</p>
-            </div>
-            <div className="card-reveal">
-              <span className="card-title grey-text text-darken-4">Think Young<i className="material-icons right">close</i></span>
-              <p>Deakin is a young, vibrant and cutting-edge university with great ambitions, and it’s our people that are the embodiment of our culture and the essence of who we are as a brand.</p>
-            </div>
+            <img alt="adv-img" className="activator" src="https://mumbrella.com.au/wp-content/uploads/2016/09/18_Deakin_Hero1_Billboard_8x13-1.jpg" />
           </div>
+          <div className="card-content">
+            <span className="card-title activator grey-text text-darken-4">Deakin university<i className="material-icons right">more_vert</i></span>
+            <p>Think Young</p>
+          </div>
+          <div className="card-reveal">
+            <span className="card-title grey-text text-darken-4">Think Young<i className="material-icons right">close</i></span>
+            <p>Deakin is a young, vibrant and cutting-edge university with great ambitions, and it’s our people that are the embodiment of our culture and the essence of who we are as a brand.</p>
+          </div>
+        </div>
 
-          );
-        }
+      );
+    }
     else {
       return (
         <div></div>
-          );
-        }
-      }
+      );
     }
-    export default Cards;
-    
-    
+  }
+}
+export default Cards;
+
+
