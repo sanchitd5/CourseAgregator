dec924f667d4bc35f862a1eadfc3e3e740f1a1ea
Vibhav-deo <vdeo@deakin.edu.au>
2019-06-03 17:45:11 +1000

    added new api's accordin to current backend

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,38 +1,93 @@
 import { axiosClient } from 'index.js';
+import axios from 'axios';
 
 class API {
 
   // POST requests
 
-  registerUser (data) {
+  registerUser(data) {
     axiosClient.post("user/register", {
       firstName: data.firstName,
       lastName: data.lastName,
       emailId: data.emailId,
       password: data.password,
     })
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => console.log(error));
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => console.log(error));
   }
 
 
-  
-  getCourses=(stateHandler) =>{
-    axiosClient.get("https://launchpad-red.au-syd.mybluemix.net/agregator/api/courses").then((response)=>
-    stateHandler({catalogue: response.data.courses,filteredCourses:response.data.courses})
-    );
-  }
-  getAgents=(stateHandler) =>{
-    axiosClient.get("https://launchpad-red.au-syd.mybluemix.net/agregator/api/agentprofile").then((response)=>
-    stateHandler({agents: response.data.agents})
+
+  // getCourses = (stateHandler) => {
+  //   axiosClient.get("http://localhost:8000​/api​/course​/v1​/getcourses​/").then((response) => {
+  //     console.log('[COURSE API]', response)
+  //     stateHandler({courses: response})
+  //   }
+  //   );
+  // }
+  // getAgents = (stateHandler) => {
+  //   axiosClient.get("https://launchpad-red.au-syd.mybluemix.net/agregator/api/agentprofile").then((response) => {
+  //     console.log('[AGENTS]', response.data.agents)
+  //     stateHandler({ agents: response.data.agents })
+  //   }
+  //   ).catch(e => {
+  //     console.log(e);
+  //   });
+  // }
+  getCourses = (stateHandler) => {
+    axiosClient.get("/course/v1/getcourses").then((response) => {
+      //console.log('[COURSES]>>>>>>>>>>>>',response.data.data)
+      stateHandler({ courseData: response.data.data })
+    }
     ).catch(e => {
-    console.log(e);
-});
+      console.log(e);
+    });
+  }
+  getStudents = (stateHandler) => {
+    axiosClient.get("student/v1/getallstudents").then((response) => {
+      //console.log('[STUDENTS]',response.data.data)
+      stateHandler({ students: response.data.data })
+    }
+    ).catch(e => {
+      console.log(e);
+    });
   }
 
+  getAgents = (stateHandler) => {
+    axiosClient.get("agent/v1/getallagents").then((response) => {
+      //console.log('[AGENTS]',response.data.data)
+      stateHandler({ agents: response.data.data })
+    }
+    ).catch(e => {
+      console.log(e);
+    });
+  }
+
+  studentLogin = (email, password,loginUser) => {
+    axiosClient.post("student/v1/login", {
+      email: email,
+      password: password
+    })
+      .then((response) => {
+        loginUser(true)
+      })
+      .catch((error) => console.log(error));
+  }
+  agentLogin = (email, password,stateHandler) => {
+    axiosClient.post("agent/v1/login", {
+      email: email,
+      password: password
+    })
+      .then((response) => {
+        stateHandler({ login: response.data.statusCode })
+      })
+      .catch((error) => console.log(error));
+  }
 }
+  
+
 
 const instance = new API();
 export default instance;
