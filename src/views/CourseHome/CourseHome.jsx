e32a8f01a31cb81908602e25e0a666bdc96fbc62
Vibhav-deo <vdeo@deakin.edu.au>
2019-06-25 15:24:40 +1000

    filteration Implemented **Note search functionality does not work **

--- src/views/CourseHome/CourseHome.jsx
+++ src/views/CourseHome/CourseHome.jsx
@@ -1,5 +1,6 @@
 ﻿import React, { Component } from 'react';
-import Cards from 'components/cards/cards.jsx';
+import UniversityCard from 'components/cards/universityCards.jsx';
+import AdvertisementCard from 'components/cards/advertisementCards.jsx';
 import LoadingComponent from 'components/loading/loading.jsx';
 import API from 'helpers/api.js';
 import M from 'materialize-css';
@@ -12,12 +13,12 @@ class CourseHome extends Component {
       backgroundImage: 'url()',
       catalogue: [],
       agents: [],
-      search: '',
+      search: null,
       tempcatalogue: [],
-      university: "true",
-      fees: "true",
-      degreelevel: "true",
-      country: "true",
+      university: null,
+      fees: null,
+      degreelevel: null,
+      country: null,
       filterationFlag: 0,
       searchIsHidden: true,
       deskSearchIsHidden: true,
@@ -27,10 +28,15 @@ class CourseHome extends Component {
       tempDegree: [],
       tempFee: [],
       tempCountry: [],
-      courseData: []
+      courseData: [],
+      interestedCourseId: [],
+      filtersLoaded: false,
+      searchedCourse: 0,
     };
     window.localStorage.pageCourseType = this.props.location.feild;
     this.loadFilters = this.loadFilters.bind(this);
+    this.courseFilter = this.courseFilter.bind(this);
+    this.searchCourse = this.searchCourse.bind(this);
     this.filterButtonInit();
     this.filteration = this.filteration.bind(this);
   }
@@ -85,26 +91,77 @@ class CourseHome extends Component {
 
   }
 
-  courseFilter = (filteredCourses) => {
-    filteredCourses = this.state.catalogue.filter(
-      (catalogue) => {
-        if (this.state.country === "true" || catalogue.Country === this.state.country) {
-          if (this.state.fees === "true" || catalogue.Fees === this.state.fees) {
-            if (this.state.degreelevel === "true" || catalogue.degreelevel === this.state.degreelevel) {
-              if (this.state.university === "true" || catalogue.University === this.state.university) {
-                if (catalogue.coursetype === this.state.pageCourseType) {
-                  if (this.state.search === '' || catalogue.Title.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1) {
-                    return catalogue.Title;
-                  }
-                }
-              }
-            }
+  courseFilter() {
+    let filteredCourse;
+    this.state.courseData.forEach(course => {
+      if (this.state.university !== null) {
+        if (this.state.country !== null) {
+          if (this.state.university === course.university.name && this.state.country === course.university.location) {
+            filteredCourse = course
+          }
+        }
+        else if (this.state.fees !== null) {
+          if (this.state.university === course.university.name && this.state.fees === course.fees) {
+            filteredCourse = course
+          }
+        }
+        else if (this.state.degreelevel !== null) {
+          console.log('------------------------->')
+          if (this.state.university === course.university.name && this.state.degreelevel === course.courseLevel) {
+            filteredCourse = course
           }
         }
       }
-    );
-    this.setState({ tempcatalogue: filteredCourses });
-    this.setState({ filterationFlag: 1 });
+      else if (this.state.country !== null) {
+        if (this.state.university !== null)
+          if (this.state.country === course.university.location && this.state.university === course.university.name) {
+            filteredCourse = course
+          }
+          else if (this.state.fees !== null) {
+            if (this.state.country === course.university.location && this.state.fees === course.fees) {
+              filteredCourse = course
+            }
+          }
+          else if (this.state.degreelevel !== null)
+            if (this.state.country === course.university.location && this.state.degreelevel === course.courseLevel) {
+              filteredCourse = course
+            }
+      }
+      else if (this.state.fees !== null) {
+        if (this.state.university !== null)
+          if (this.state.fees === course.fees && this.state.university === course.university.name) {
+            filteredCourse = course
+          }
+          else if (this.state.country !== null)
+            if (this.state.fees === course.fees && this.state.country === course.university.location) {
+              filteredCourse = course
+            }
+            else if (this.state.degreelevel !== null)
+              if (this.state.fees === course.fees && this.state.degreelevel === course.courseLevel) {
+                filteredCourse = course
+              }
+      }
+      else if (this.state.degreelevel !== null) {
+        if (this.state.university !== null)
+          if (this.state.fees === course.fees && this.state.university === course.university.name) {
+            filteredCourse = course
+          }
+          else if (this.state.country !== null)
+            if (this.state.fees === course.fees && this.state.country === course.university.location) {
+              filteredCourse = course
+            }
+            else if (this.state.fees !== null)
+              if (this.state.fees === course.fees && this.state.degreelevel === course.courseLevel) {
+                filteredCourse = course
+              }
+      }
+    })
+    console.log('[FILTERED COURSE]', filteredCourse)
+
+    this.setState({
+      tempcatalogue: filteredCourse,
+      filterationFlag: 1
+    });
 
   }
 
@@ -115,35 +172,33 @@ class CourseHome extends Component {
     let filterCountry = [];
     let filterDegree = [];
 
-    this.state.catalogue.forEach((value, key) => {
-
-      if (value.coursetype === this.state.pageCourseType) {
-        if (filterUni.indexOf(value.University) === -1) {
-          filterUni.push(value.University)
-        }
-        if (filterDegree.indexOf(value.degreelevel) === -1) {
-          filterDegree.push(value.degreelevel)
-        }
-        if (filterExp.indexOf(value.experience) === -1) {
-          filterExp.push(value.experience)
-        }
-        if (filterFee.indexOf(value.Fees) === -1) {
-          filterFee.push(value.Fees)
-        }
-        if (filterCountry.indexOf(value.Country) === -1) {
-          filterCountry.push(value.Country)
-        }
-      }
+    this.state.courseData.forEach((value, key) => {
+      if (filterDegree.includes(value.courseLevel) === false)
+        filterDegree.push(value.courseLevel)
+      if (filterUni.includes(value.university.name) === false)
+        filterUni.push(value.university.name)
+      if (filterCountry.includes(value.university.location) === false)
+        filterCountry.push(value.university.location)
+      if (filterFee.includes(value.fees) === false)
+        filterFee.push(value.fees)
     });
-    this.setState({ tempUni: filterUni });
-    this.setState({ tempExp: filterExp });
-    this.setState({ tempCountry: filterCountry });
-    this.setState({ tempDegree: filterDegree });
-    this.setState({ tempFee: filterFee });
+    if (this.state.filtersLoaded === false)
+      this.setState({
+        tempUni: filterUni,
+        tempExp: filterExp,
+        tempCountry: filterCountry,
+        tempDegree: filterDegree,
+        tempFee: filterFee,
+        filtersLoaded: true
+      });
   }
 
   filteration = (data) => {
-
+    console.log('[DATA]', data.target.value)
+    if (this.state.filterationFlag === 1)
+      this.setState({
+        filterationFlag: 0
+      })
     if (data.target.id === "University" | data.target.id === "University-sidenav") {
       this.setState({ university: data.target.value });
     }
@@ -158,18 +213,23 @@ class CourseHome extends Component {
     }
   }
 
-
+  searchCourse() {
+    if (this.state.search) {
+      this.setState({
+        searchedCourse: 1
+      })
+    }
+  }
   render() {
-    console.log('[COURSES]', this.stat)
     if (this.state.courseData.length === 0) return <LoadingComponent />;
     if (this.state.filterationFlag === 0) {
-      console.log(this.state.temppageCourseType)
+      console.log(this.state.courseData)
 
 
-      this.courseFilter();
+
       this.loadFilters();
     }
-
+    console.log('ACTIVE FILTERS', this.state.activeFilters)
     document.body.style.backgroundImage = this.state.backgroundImage;
     return (
 
@@ -179,11 +239,11 @@ class CourseHome extends Component {
           {!this.state.searchIsHidden &&
             <div className=" row section card-panel hoverable hide-on-large-only">
               <div className="valign-wrapper">
-                <div className="col s8">
-                  <input type="text" value={this.state.search} onChange={this.updateSearch} placeholder="Search Courses" />
+                <div className="col s7">
+                  <input type="text" onChange={this.updateSearch} placeholder="Search Courses" />
                 </div>
-                <div className="col s4">
-                  <a href="#!" onClick={this.courseFilter} className=" btn-small red white-text">Search</a>
+                <div className="col s5">
+                  <a href="#!" onClick={this.searchCourse} className=" btn-small red white-text">Search</a>
                 </div>
               </div>
               <div className="col s12 left-align">
@@ -258,7 +318,7 @@ class CourseHome extends Component {
           }
           <div className="row">
             {!this.state.deskSearchIsHidden && <div className="valign-wrapper card-panel row hide-on-med-and-down hoverable"><div className="col l11 s0 m0"><input value={this.state.search} onChange={this.updateSearch} placeholder="Search Courses" /></div>
-              <div className="col l1 s0 m0"><a href="#!" onClick={this.courseFilter} className="btn red white-text">Search</a></div></div>}
+              <div className="col l1 s0 m0"><a href="#!" onClick={this.searchCourse} className="btn red white-text">Search</a></div></div>}
             <div className="card-panel pin-top col l3 s0 m0 hide-on-med-and-down">
               <p className="flow-text">Filters</p>
               <div className="divider"></div><br />
@@ -319,18 +379,30 @@ class CourseHome extends Component {
               </div>
             </div>
 
-            <div className="col l7 m12 s12">
+            <div className="col l6 m12 s6">
+
               <div className="">
                 {
-                  this.state.courseData.map((value, i) => {
+                  this.state.filterationFlag === 1 ? this.state.tempcatalogue !== undefined ? <UniversityCard data={this.state.tempcatalogue} parentState={this.state} parentStateHandler={this.stateHandler} ></UniversityCard> : <p>No courses match your filters</p> : this.state.courseData.map((value, i) => {
                     return (
-                      <Cards key={i} data={value} Ctype="Cdesc" previousField={this.state.pageCourseType} parentStateHandler={this.stateHandler} parentState={this.state}/>
+                      <UniversityCard data={value} key={i} parentState={this.state} parentStateHandler={this.stateHandler} ></UniversityCard>
                     )
                   })
                 }
+                {
+                  this.state.searchedCourse !== 0 ? this.state.courseData.forEach((course, i) => {
+                    if (course.name.includes(this.state.search)) {
+                      return (
+                        <UniversityCard data={course} parentState={this.state} parentStateHandler={this.stateHandler} ></UniversityCard>
+                      )
+                    }
+                    else
+                      console.log('[NOT FOUND]', course.university.name.includes(this.state.search))
+                  }) : null
+                }
               </div>
               {
-                (!this.state.tempcatalogue.length ?
+                (!this.state.courseData.length ?
                   <div>
                     <p className="flow-text">
                       Sorry, No Courses with that name found.
@@ -339,10 +411,8 @@ class CourseHome extends Component {
                   : '')
               }
             </div>
-            <div className="col l2 m0 s0 hide-on-med-and-down">
-              <Cards Ctype="advert" />
-              <Cards Ctype="advert" />
-              <Cards Ctype="advert" />
+            <div className="col l3 s0 m0 hide-on-med-and-down">
+              <AdvertisementCard></AdvertisementCard>
             </div>
           </div>
         </div>
@@ -364,3 +434,22 @@ class CourseHome extends Component {
 
 export default CourseHome;
 
+// this.state.courseData.map((value, i) => {
+//   console.log('==================>', this.state.tempcatalogue)
+//   if (this.state.university !== null && value.university.name === this.state.tempcatalogue)
+//     return (
+//       <UniversityCard data={value} key={i} parentState={this.state} parentStateHandler={this.stateHandler} ></UniversityCard>
+//     )
+//   if (this.state.degreelevel !== null && value.courseLevel === this.state.tempcatalogue)
+//     return (
+//       <UniversityCard data={value} key={i} parentState={this.state} parentStateHandler={this.stateHandler} ></UniversityCard>
+//     )
+//   if (this.state.fees !== null && value.fees === this.state.tempcatalogue)
+//     return (
+//       <UniversityCard data={value} key={i} parentState={this.state} parentStateHandler={this.stateHandler} ></UniversityCard>
+//     )
+//   if (this.state.country !== null && value.university.location === this.state.tempcatalogue)
+//     return (
+//       <UniversityCard data={value} key={i} parentState={this.state} parentStateHandler={this.stateHandler} ></UniversityCard>
+//     )
+// })
